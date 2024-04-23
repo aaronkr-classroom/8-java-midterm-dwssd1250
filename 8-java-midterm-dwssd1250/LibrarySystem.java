f991af40ff0905e4e324e746c7b3304d0d748e8d
dwssd1250 <162276812+dwssd1250@users.noreply.github.com>
2024-04-23 16:49:47 +0900

    Add files via upload

---
+++ 8-java-midterm-dwssd1250/LibrarySystem.java
@@ -0,0 +1,92 @@
+public class LibrarySystem {
+	// 변수 정의
+	private Book[] booklist;
+	private static int bookCount = 0;
+	private static final int NUM_BOOK = 10;
+	// 생성자 정의
+	public LibrarySystem() {
+	    this.booklist = new Book[NUM_BOOK]; // 생성자
+	}
+
+	
+	
+	public void addBook(Book book) {
+		// 책 추가
+		// 도움을 위해 Cart.java의 insertBook(Book book)을 참조하세요
+		booklist[bookCount++] = new LibraryBook(book);
+	}
+	
+	public void removeBook(String title) {
+    	// `for` LibrarySystem의 모든 Book
+		for ( int i = 0; i < bookCount; i++ ) {
+
+      		// `if` 책 제목이 발견되면
+			if ( booklist[i].getTitle().equals(title)) {
+				
+                // 찾은 책부터 시작하여 모든 요소를 왼쪽으로 이동합니다.
+				for ( int j = i; j < bookCount - 1; j++ ) {
+					booklist[j] = booklist[j+1];
+				}
+        
+				// 마지막 요소를 null로 설정하고 bookCount를 줄입니다.
+				booklist[--bookCount] = null;
+				System.out.println("라이브러리에서 '" + title + "' 삭제했습니다.");
+        
+				return; // 책 제거 후 종료 방법
+			}
+		}
+		// 찾을 수 없으면 메시지를 인쇄합니다.
+		System.out.println("# 오류: '" + title + "' 못 찾았습니다!");
+	}
+	
+	
+	public void borrowBook(String title) {
+        for ( int i = 0; i < bookCount; i++ ) { // `for` LibrarySystem의 모든 Book
+        	if ( booklist[i].getTitle().equals(title)) { // `if` 책 제목이 발견되면
+                if(booklist[i].isAvailable()) {  // `if` 책이 있다면 
+                	booklist[i].setAvailable(false);
+                	System.out.println("빌렸습니다.");
+                	// setAvailable을 false로 설정하고 메시지를 출력합니다.
+                }
+                else {  // `else` 책이 없다면
+                	System.out.println("이미 빌린 책입니다.");
+                	 // 메시지를 인쇄하다
+                }
+				return;
+			}
+		}
+		System.out.println("# 오류: '" + title + "' 못 찾았습니다!");
+	}
+	
+	public void returnBook(String title) {
+		for ( int i = 0; i < bookCount; i++ ) { // `for` LibrarySystem의 모든 Book
+			if ( booklist[i].getTitle().equals(title)) { // `if` 책 제목이 발견되면
+				if(!booklist[i].isAvailable()) {  // `if` 책이 있다면 
+                	booklist[i].setAvailable(true);
+                	System.out.println("반납했습니다.");
+                	// setAvailable을 false로 설정하고 메시지를 출력합니다.
+                }
+                else {  // `else` 책이 없다면
+                	System.out.println("이미 반납했습니다.");
+                	 // 메시지를 인쇄하다
+                }
+				return;
+			}
+		}
+		System.out.println("# 오류: '" + title + "' 못 찾았습니다!");
+	}
+	
+	public void displayAllBooks() {
+		// 책 목록 출력 (책이름, 저자, 출판년도, 대출가능여부 출력)
+		for(int i=0; i<bookCount; i++) {
+			System.out.print(booklist[i].getTitle() + "|");
+			System.out.print(booklist[i].getAuthor() + "|");
+			System.out.print(booklist[i].getYear() + "|");
+			System.out.print(booklist[i].isAvailable() + "|");
+			System.out.print("");
+		}
+    
+        
+    }
+
+}
